587eb539933334f810bbe366f706389db37f34d0
XFantasy88 <erencote@gmail.com>
2024-06-06 09:01:46 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import "@toob-finance/widget/dist/style.css"
 function App() {
   return (
     <div>
-      <SwapWidget amount="1" inputToken="ETH" outputToken="WBTC" />
+      <SwapWidget
+        amount="1"
+        inputToken="ETH"
+        outputToken="USDT"
+        theme={{ colorPrimaryText: "red", colorPrimaryBackground: "yellow" }}
+      />
     </div>
   )
 }
